7270f14d36c7ce2c7cd876f7704bb0f46d398f67
Matthew White <matthewzwartswhite@gmail.com>
2019-06-28 18:24:07 -0400

    Removed style-loader and added sass to index.html
    *Removed style-loader as it's lazy loading features weren't needed
    
    *Replaced placeholder images with updated url

--- devserver.config.js
+++ devserver.config.js
@@ -22,7 +22,7 @@ module.exports = {
     rules: [
       {
         test: /\.(c|sa)ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [ 'css-loader', 'sass-loader']
       },
       {
         test: /\.pug$/,
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 console.log('hello world!');
 import 'sanitize.css';
 import './index.pug';
-import './style.sass';
 
 // import './index/spash.js';
 import './index/contact.js';
