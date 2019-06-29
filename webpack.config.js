67e813932637eac80ae28c07581f7def2359b920
Matthew White <matthewzwartswhite@gmail.com>
2019-06-29 00:20:03 -0400

    Added clean-css-loader, a css minifier
    *Added clean-css-loader and set it to run only on production mode
    
    *To accomplish this, webpack.config was changed to return a function rather than a object. This change shouldn't affect anything else

--- webpack.config.js
+++ webpack.config.js
@@ -2,64 +2,74 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(c|sa)ss$/,
-        use: ['css-loader', 'sass-loader']
-      },
-      {
-        test: /\.pug$/,
-        use: [
-          'file-loader?name=index.html',
-          'extract-loader',
-          'html-loader?interpolate=true',
-          'pug-html-loader'
-        ]
-      },
-      // loader for large images, uses svg traces to load placeholders
-      {
-        test: /\.(?:png|jpe?g|gif)$/,
-        include: [path.resolve(__dirname, 'src/assets/large')],
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'assets/'
+
+module.exports = (env, argv) => {
+  console.log(argv.mode)
+  
+  const cssLoaderList = ['css-loader', 'sass-loader'];
+  if (argv.mode === 'production') {
+    cssLoaderList.splice(1,0,'clean-css-loader');
+   };
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(c|sa)ss$/,
+          use: cssLoaderList
+        },
+        {
+          test: /\.pug$/,
+          use: [
+            'file-loader?name=index.html',
+            'extract-loader',
+            'html-loader?interpolate=true',
+            'pug-html-loader'
+          ]
+        },
+        // loader for large images, uses svg traces to load placeholders
+        {
+          test: /\.(?:png|jpe?g|gif)$/,
+          include: [path.resolve(__dirname, 'src/assets/large')],
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'assets/'
+              }
+            },
+            {
+              loader: 'image-webpack-loader'
             }
-          },
-          {
-            loader: 'image-webpack-loader'
-          }
-        ]
-      },
-      // loader for small images
-      {
-        test: /\.(?:png|jpe?g|gif)$/,
-        include: [path.resolve(__dirname, 'src/assets/small')],
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'assets/'
+          ]
+        },
+        // loader for small images
+        {
+          test: /\.(?:png|jpe?g|gif)$/,
+          include: [path.resolve(__dirname, 'src/assets/small')],
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'assets/'
+              }
+            },
+            {
+              loader: 'image-webpack-loader'
             }
-          },
-          {
-            loader: 'image-webpack-loader'
-          }
-        ]
-      },
-      // loader for svgs
-      {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader'
-      }
-    ]
+          ]
+        },
+        // loader for svgs
+        {
+          test: /\.svg$/,
+          loader: 'svg-inline-loader'
+        }
+      ]
+    }
   }
 };
