55f25c79cdc724103b9e58647d2a3b25c3150f13
Matthew White <matthewzwartswhite@gmail.com>
2019-07-03 14:02:18 -0400

    Updated Client & services section
    * Gave section a slight visual overhaul:
       ** Content margins now in line with the rest of the content
       ** Description starts with a header
       ** A responsive 2:1 to stacked 1:1 layout
    
    * Changed how tabs worked and made them arguably worse, now uses all original code and works through custom html attributes, remove previous code
    
    * Small spacing adjustments all over the page, as a result of the font changing
    
    * Added some comments to css to make it more obvious which part of the css does what
    
    * removed some redundant styling from the testimonials section

--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ import './index.pug';
 
 // import './index/spash.js';
 import './index/contact.js';
-
-import openService from './index/openservice.js';
-window.openService = openService;
+import './index/client-services.js';
---
+++ src/index/client-services.js
@@ -0,0 +1,34 @@
+// cache elements
+var $selectors = document.getElementsByClassName('service'),
+  $tabs = document.getElementsByClassName('tab-content'),
+  $selectorLinks = document.querySelectorAll('[tab]');
+
+// apply class to selected tab and remove class from all other tabs
+var chooseTab = function(select) {
+  for (var i = 0; i < $selectors.length; i++) {
+    $selectors[i].classList.remove('active');
+    $tabs[i].classList.remove('active');
+  }
+  $selectors[select].classList.add('active');
+  $tabs[select].classList.add('active');
+  
+};
+
+// add event listeners to elements with the class '.selectors'
+for (var i = 0; i < $selectors.length; i++) {
+  // use a javascript closure to pass i into the callback
+  (function(i) {
+    $selectors[i].addEventListener('mouseup', function() {
+      chooseTab(i)
+    });
+  })(i)
+}
+
+// add event listeners to elements with the attribute tab
+for (var i = 0; i < $selectorLinks.length; i++) {
+  (function(i) {
+    $selectorLinks[i].addEventListener('mouseup', function() {
+      chooseTab($selectorLinks[i].getAttribute('tab'))
+    });
+  })(i)
+}
