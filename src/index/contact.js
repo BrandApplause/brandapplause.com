f62d2ba49071f4857b580207e27277a5fe050de1
Matthew White <matthewzwartswhite@gmail.com>
2019-07-03 23:15:15 -0400

    added a success and failure animation to email form
    * Form now has minor animations for sending and failing to send, may add a way to see error info on the page, and I may add a way to reject empty letters in the future. For now this is enough though

--- src/index/contact.js
+++ src/index/contact.js
@@ -1,6 +1,7 @@
 // https://usebasin.com/f/476f85219509
 
 var $form = document.getElementById('email'),
+  $paper= document.getElementsByClassName('paper'),
   $submit = document.getElementById('submit');
 
 // close envelope
@@ -26,12 +27,35 @@ $submit.addEventListener('mouseup', function(event) {
     console.log(xhr);
     var response = JSON.parse(xhr.response);
     if (xhr.status === 200) {
-      $submit.innerHTML = "Success";
+      $submit.innerHTML = "Sent!";
+      //activate animation, then remove it when finished
+      $form.classList.add('sent');
+      setTimeout(function() {
+        $form.classList.remove('sent');
+        //open the letter once sent
+        $submit.addEventListener('mouseleave', notSending);
+        $form.classList.remove('sending');
+      }, 1450);
+      // reset button
+      $paper[0].addEventListener('mouseover', function() {
+        $submit.innerHTML = "Submit";
+        $paper[0].removeEventListener('mouseover', this);
+      })
       $form.reset();
     } else {
+      //change button to an error button
       $submit.innerHTML = "Error";
-      console.log(response.error);
-      $submit.addEventListener('mouseleave', notSending);      
+      $submit.classList.add('error');
+      //log error in console for some reason
+      console.error(response.error);
+      // re-add event listener so people can maybe fix their message 
+      $submit.addEventListener('mouseleave', notSending);
+      // then if they mouse over the message remove the error button
+      $paper[0].addEventListener('mouseover', function() {
+        $submit.innerHTML = "Submit";
+        $submit.classList.remove('error');
+        $paper[0].removeEventListener('mouseover', this);
+      })
     }
   };
   xhr.send(formData);
