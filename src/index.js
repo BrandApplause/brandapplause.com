0562fc08a4b60a0428a0206321f7df463753b5d4
MattWThomas <macravin@gmail.com>
2019-07-11 11:34:11 -0500

    Preliminary lazyloading of youtube video

--- src/index.js
+++ src/index.js
@@ -38,3 +38,9 @@ $links.forEach(function(link) {
     });
   })
 });
+
+//---- Lazy loading
+import LazyLoad from "vanilla-lazyload";
+
+const lazyLoadOptions = {elements_selector: ".lazy"};
+const pageLazyLoad = new LazyLoad(lazyLoadOptions);
