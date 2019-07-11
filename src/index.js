f5cb3a4ae0f2c4b02459b65218a920c4d31c9cf7
Matthew White <matthewzwartswhite@gmail.com>
2019-07-10 22:15:54 -0400

    Added smooth scrolling
    * Added smooth scrollint that will automatically target any link targeting a elements #id
    
    * Made the bouncing scroll down indicator on the splash page scroll you down if you click on it

--- src/index.js
+++ src/index.js
@@ -5,3 +5,26 @@ import './index.pug';
 // import './index/spash.js';
 import './index/client-services.js';
 import './index/contact.js';
+
+//----------Site wide JS
+
+//---- smooth scroller
+//get list of elements that link to elements with ids
+var $links = Array.from(document.querySelectorAll('[href]')).filter(function(link) {
+  return link.getAttribute('href').charAt(0) === '#';
+});
+$links.forEach(function(link) {
+  //prevent page from jumping to linked element as would be normal
+  link.addEventListener('click', function(ev) {
+    ev.preventDefault();
+  });
+  //make link smoothscroll (why is this event listener 'mouseup' and the previous 'click'? because I prefer mouseup for interactable elements so the user can mouse off the element if they change their mind, and click is I think needed because thats what triggers the links behavior)
+  link.addEventListener('mouseup', function() {
+    var target = document.getElementById(link.getAttribute('href').slice(1));
+    window.scrollTo({
+      left: 0 + window.scrollX,
+      top: target.getBoundingClientRect().top - 54 + window.scrollY, //-54px for the nav bar
+      behavior: 'smooth'
+    });
+  })
+});
