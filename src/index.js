2042e424a9518cac981d5580a9e6ba7cb198db1a
Chris K. Thomas <135461249+chriskthomas@users.noreply.github.com>
2023-06-06 20:00:39 +0000

    simplify and update

--- src/index.js
+++ src/index.js
@@ -1,47 +1,4 @@
 /* jshint esversion: 6 */
-console.log('hello world!');
-import './index.pug';
-import './404.pug';
-
 // import './index/spash.js';
-import './index/client-services.js';
-import './index/contact.js';
-
-//----------Site wide JS
-
-//---- smooth scroller
-//get list of elements that link to elements with ids
-var $links = Array.from(document.querySelectorAll('[href]')).filter(function(link) {
-  return link.getAttribute('href').charAt(0) === '#';
-});
-$links.forEach(function(link) {
-  //prevent page from jumping to linked element as would be normal
-  link.addEventListener('click', function(ev) {
-    ev.preventDefault();
-  });
-  //make link smoothscroll (why is this event listener 'mouseup' and the previous 'click'? because I prefer mouseup for interactable elements so the user can mouse off the element if they change their mind, and click is I think needed because thats what triggers the links behavior)
-  link.addEventListener('mouseup', function() {
-    var target = document.getElementById(link.getAttribute('href').slice(1)),
-      targetPosition = 0;
-    // test if there is no target element, ie the logo, and makes it go to the top of the page
-    if (target === null) {
-      targetPosition = 0
-    // then test if the window is thinner than 700px, so you know if the nav bar would overlap what you were scrolling to, if it would, scroll to slightly above the element
-    } else if (window.innerWidth <= 700) {
-      targetPosition = target.getBoundingClientRect().top + window.scrollY;
-    } else {
-      targetPosition = target.getBoundingClientRect().top - 54 + window.scrollY;
-    }
-    window.scrollTo({
-      left: 0 + window.scrollX,
-      top: targetPosition,
-      behavior: 'smooth'
-    });
-  })
-});
-
-//---- Lazy loading
-import LazyLoad from "vanilla-lazyload";
-
-const lazyLoadOptions = {elements_selector: ".lazy"};
-const pageLazyLoad = new LazyLoad(lazyLoadOptions);
+import "./index/client-services.js";
+import "./index/contact.js";
--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,40 @@
 /* jshint esversion: 6 */
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const PugPlugin = require("pug-plugin");
 
-
-module.exports = (env, argv) => {
-  console.log(argv.mode)
-  
-  const cssLoaderList = ['css-loader', 'sass-loader'];
-  if (argv.mode === 'production') {
-    cssLoaderList.splice(1,0,'clean-css-loader');
-   };
-
-  return {
-    entry: './src/index.js',
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(c|sa)ss$/,
-          use: cssLoaderList
-        },
-        {
-          test: /\.pug$/,
-          use: [
-		    {
-		      loader: 'file-loader',
-		      options: {
-		        name: '[name].html',
-		      }
-		    },
-            'extract-loader',
-            'html-loader?interpolate=true',
-            'pug-html-loader'
-          ]
-        },
-        // loader for large images, uses svg traces to load placeholders
-        {
-          test: /\.(?:png|jpe?g|gif)$/,
-          include: [path.resolve(__dirname, 'src/assets/large')],
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                outputPath: 'assets/'
-              }
-            },
-            {
-              loader: 'image-webpack-loader'
-            }
-          ]
-        },
-        // loader for small images
-        {
-          test: /\.(?:png|jpe?g|gif)$/,
-          include: [path.resolve(__dirname, 'src/assets/small')],
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                outputPath: 'assets/'
-              }
-            },
-            {
-              loader: 'image-webpack-loader'
-            }
-          ]
-        },
-        // loader for svgs
-        {
-          test: /\.svg$/,
-          loader: 'svg-inline-loader'
-        }
-      ]
-    }
-  }
+module.exports = {
+  entry: {
+    index: "./src/index.pug",
+    404: "./src/404.pug",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new PugPlugin(), // rendering of Pug files defined in Webpack entry
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(c|sa)ss$/,
+        use: ["css-loader", "sass-loader"],
+      },
+      {
+        test: /\.pug$/,
+        loader: PugPlugin.loader, // Pug loader,
+      },
+      // loader for large images, uses svg traces to load placeholders
+      {
+        test: /\.(?:png|jpe?g|gif)$/,
+        type: "asset/resource",
+      },
+      // loader for svgs
+      {
+        test: /\.svg$/,
+        type: "asset/source",
+      },
+    ],
+  },
 };
